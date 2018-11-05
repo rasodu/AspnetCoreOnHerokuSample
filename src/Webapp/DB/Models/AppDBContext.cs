691275568552e8fd3446b304a6b44b1cf416c1c1
Abhijit Amin <abhijitramin@gmail.com>
2018-11-05 09:46:26 -0500

    Add model and db context.

---
+++ src/Webapp/DB/Models/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Webapp.DB.Models
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TestTableRecord> TestTableRecords { get; set; }
+    }
+}
