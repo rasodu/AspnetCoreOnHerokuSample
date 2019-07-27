7e324e500092e8cd0cd66ecdff47effdac6440f1
Abhijit Amin <abhijitramin@gmail.com>
2019-07-27 15:28:49 -0400

    Fix new relic package download URL.

--- scripts/install-new-relic.sh
+++ scripts/install-new-relic.sh
@@ -4,6 +4,6 @@ set -e
 set -o verbose
 echo "This command will be executed during building Web image. It will install New Relic. Define 'NEW_RELIC_LICENSE_KEY' and 'NEW_RELIC_APP_NAME' for your applicaiton."
 apt-get update && apt-get install wget
-wget -O newrelic-netcore20-agent.deb 'https://download.newrelic.com/dot_net_agent/latest_release/newrelic-netcore20-agent_8.12.216.0_amd64.deb'
+wget -O newrelic-netcore20-agent.deb 'https://download.newrelic.com/dot_net_agent/latest_release/newrelic-netcore20-agent_8.17.438.0_amd64.deb'
 dpkg -i newrelic-netcore20-agent.deb
 rm newrelic-netcore20-agent.deb
