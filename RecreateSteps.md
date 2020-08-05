f685bae0902449dcec3174db5fabb4358c39bb70
Abhijit Amin <abhijitramin@gmail.com>
2020-08-05 00:59:49 -0400

    Readd timehosted service.

---
+++ RecreateSteps.md
@@ -0,0 +1,4 @@
+## Additional stuff installed
+- [Swagger UI](https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-3.1&tabs=visual-studio)
+- [HostedServices](https://github.com/rasodu/AspnetCoreOnHerokuSample/pull/12/files)
+ 
--- src/Webapp/Startup.cs
+++ src/Webapp/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Webapp.HostedServices;
 
 namespace Webapp
 {
@@ -33,6 +34,9 @@ namespace Webapp
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
+
+            //register hosted services
+            services.AddHostedService<TimedHostedService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
